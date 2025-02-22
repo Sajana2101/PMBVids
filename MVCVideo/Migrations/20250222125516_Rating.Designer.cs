c74bb43ee638ac0e6ed8ed0deae71ea6218f5a7b
Sajana21 <ST10433940@vcconnect.edu.za>
2025-02-22 14:57:50 +0200

    Added a new field

--- MVCVideo/Migrations/20250222125516_Rating.Designer.cs
+++ MVCVideo/Migrations/20250222125516_Rating.Designer.cs
@@ -4,6 +4,7 @@ using MVCVideo.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MVCVideo.Migrations
 {
     [DbContext(typeof(MVCVideoContext))]
-    partial class MVCVideoContextModelSnapshot : ModelSnapshot
+    [Migration("20250222125516_Rating")]
+    partial class Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
