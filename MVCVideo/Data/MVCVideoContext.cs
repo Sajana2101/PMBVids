96a5e0f5f17171caaf16376554d4fa5ab387232a
Sajana21 <ST10433940@vcconnect.edu.za>
2025-02-22 13:41:45 +0200

    Added a view

---
+++ MVCVideo/Data/MVCVideoContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCVideo.Models;
+
+namespace MVCVideo.Data
+{
+    public class MVCVideoContext : DbContext
+    {
+        public MVCVideoContext (DbContextOptions<MVCVideoContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCVideo.Models.Movie> Movie { get; set; } = default!;
+    }
+}
